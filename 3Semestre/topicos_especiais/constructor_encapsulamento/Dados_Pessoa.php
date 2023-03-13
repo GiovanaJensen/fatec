89b522fc74ea54e6beee1aa3a6931c1a0ad6a044
DE OLIVEIRA FERNANDES <giovana.fernandes2@fatec.com>
2023-03-13 09:04:36 -0400

    mudanca de nome de usuario  para Willian

--- 3Semestre/topicos_especiais/constructor_encapsulamento/Dados_Pessoa.php
+++ 3Semestre/topicos_especiais/constructor_encapsulamento/Dados_Pessoa.php
@@ -3,19 +3,19 @@
 include_once "Pessoa.class.php";
 include_once "exercicios/Dados.Complementares.class.php";
 
-$Giovana = new Pessoa(1,"Giovana",1.57,19,"01/06/2003","2° ano faculdade", 0.00);
-$Giovana_Dados_Complementares = new Dados_Complementares("solteira", "9973738927", "Av. Ana costa");
+$Willian = new Pessoa(1,"Patrick Willian", 1.75, 19, "03/05/1974", "1° ano faculdade", 756);
+$Willian_Dados_Complementares = new Dados_Complementares("solteiro", "9973738927", "Av. Ana costa");
 
 // manipulação do objeto
 
-echo nl2br("Nome: ". $Giovana->get_nome()."\n");
-echo nl2br("Código: ". $Giovana->get_codigo()."\n");
-echo nl2br("Altura: ". $Giovana->get_altura()."\n");
-echo nl2br("Idade: ". $Giovana->get_idade()."\n");
-echo nl2br("Nascimento: ". $Giovana->get_nascimento()."\n");
-echo nl2br("Escolaridade: ". $Giovana->get_escolaridade()."\n");
-echo nl2br("Salário: ". $Giovana->get_salario()."\n");
-echo nl2br("Estado Civil: ". $Giovana_Dados_Complementares->get_estado_civil()."\n");
-echo nl2br("Telefone: ". $Giovana_Dados_Complementares->get_telefone()."\n");
-echo nl2br("Endereço: ". $Giovana_Dados_Complementares->get_endereco()."\n");
+echo nl2br("Nome: ". $Willian->get_nome()."\n");
+echo nl2br("Código: ". $Willian->get_codigo()."\n");
+echo nl2br("Altura: ". $Willian->get_altura()."\n");
+echo nl2br("Idade: ". $Willian->get_idade()."\n");
+echo nl2br("Nascimento: ". $Willian->get_nascimento()."\n");
+echo nl2br("Escolaridade: ". $Willian->get_escolaridade()."\n");
+echo nl2br("Salário: ". $Willian->get_salario()."\n");
+echo nl2br("Estado Civil: ". $Willian_Dados_Complementares->get_estado_civil()."\n");
+echo nl2br("Telefone: ".  $Willian_Dados_Complementares->get_telefone()."\n");
+echo nl2br("Endereço: ".  $Willian_Dados_Complementares->get_endereco()."\n");
 ?>
