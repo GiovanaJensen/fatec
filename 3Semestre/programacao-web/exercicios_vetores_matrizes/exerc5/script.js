28c4fb3cba1b3df040d8c29f12b2d8b6ff318eab
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-03-27 16:13:37 -0300

    PPSI-3 TP01 exerc03

---
+++ 3Semestre/programacao-web/exercicios_vetores_matrizes/exerc5/script.js
@@ -0,0 +1,6 @@
+/*Faça um algoritmo que calcule a soma dos primeiros 50 números pares. Este algoritmo não recebe
+valor do teclado. Os primeiros número pares são: 2, 4, 6, ... */
+
+for(var i=2; i<50; i=i+2){
+    console.log(i);
+}
