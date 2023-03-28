a32c8ff3bc3a56907a071dc52bedc32a028da2ae
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-03-28 15:29:35 -0300

    implement function in the exercise 3

--- 3Semestre/programacao-web/tp01/exerc03/main.js
+++ 3Semestre/programacao-web/tp01/exerc03/main.js
@@ -2,15 +2,21 @@
 Implemente um programa que tenha uma matriz de ordem 2x2, peça para o usuário popular esta matriz , calcule a determinante da matriz e mostre para o usuário o resultado.
 */
 
-var matriz =[[],[]];
+function matriz(){
+    var matriz =[[],[]];
 
-for(var i=0; i<2; i++){
-    for(var j=0; j<2; j++){
-        matriz[i][j] = parseInt(prompt("Digite o valor da matriz na linha " + i + " na coluna " + j));
+    for(var i=0; i<2; i++){
+        for(var j=0; j<2; j++){
+            matriz[i][j] = parseInt(prompt("Digite o valor da matriz na linha " + i + " na coluna " + j));
+        }
     }
+
+    document.write(getDeterminant(matriz));
 }
 
+function getDeterminant(matriz){
+    var d = matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
+    return d;
+}
 
-d = matriz[0][0] * matriz[1][1] - matriz[0][1] * matriz[1][0];
-
-document.write(d);
+matriz();
