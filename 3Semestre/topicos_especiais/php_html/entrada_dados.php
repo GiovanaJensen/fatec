0b2d17fdf1270dafcf10c05033562e6f95bafbac
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-04-25 14:48:39 -0300

    separate topicos-especiais folders

--- 3Semestre/topicos_especiais/php_html/entrada_dados.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-$_POST → envio de dados por formulário;
-$_GET → envio de dados por url. Ex: http://localhost/aplicação.php?variável=xx
-*/
-
-$nome = $_POST['nome'];
-echo "Nome: $nome";
-?>
