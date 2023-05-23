e8f9374f17ab291ac9bf292dd2a197a6eea1913a
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-05-23 17:55:08 -0300

    show more information

--- 3Semestre/topicos_especiais/2bimestre/tp02/ex14/aplicaHeranca.php
+++ 3Semestre/topicos_especiais/2bimestre/tp02/ex14/aplicaHeranca.php
@@ -5,9 +5,12 @@ require_once "EmpresaSantosFC.php";
 require_once "EmpresaRenner.php";
 
 $santos = new EmpresaSantosFC("Santos FC", 788, 50);
+print "Empresa: {$santos->get_nome()} <br>\n";
+print "Produtos Vendidos: {$santos->get_produtosVendidos()} <br>\n";
+print "Valor do produto: R$ {$santos->get_valorProdutos()} <br>\n";
 $santos->calcularReceita();
-print "Receita: {$santos->get_receita()} <br>\n";
-print "Custos: {$santos->get_custos()} <br>\n";
+print "Receita: R$ {$santos->get_receita()} <br>\n";
+print "Custos: R$ {$santos->get_custos()} <br><br>\n";
 
 $produtos = [
     ['descricao' => 'Blusa', 'custo' => 100],
@@ -17,8 +20,11 @@ $produtos = [
 $custosDiretos = [200, 300];
 
 $renner = new EmpresaRenner("Renner", 50, 180,  $produtos, $custosDiretos);
+print "Empresa: {$renner->get_nome()} <br>\n";
+print "Produtos vendidos: {$renner->get_produtosVendidos()} <br>\n";
+print "Valor do produto: R$ {$renner->get_valorProdutos()} <br>\n";
 $renner->calcularReceita();
-print "Receita: {$renner->get_receita()} <br>\n";
+print "Receita: R$ {$renner->get_receita()} <br>\n";
 $custoTotal = $renner->get_custos();
 echo "Custo Total da Ordem de Produção: R$ " . $custoTotal;
 
